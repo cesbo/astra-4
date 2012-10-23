83ba4b77e9334c1717c3ed1433905cefc343d7e7
Andrey Dyldin <and@cesbo.com>
2012-10-23 17:34:57 +0600

    release

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_ 1
 
-#define ASTRA_VERSION 1
-#define ASTRA_VERSION_DEV 100
+#define ASTRA_VERSION 2
+#define ASTRA_VERSION_DEV 0
 
 #endif /* _VERSION_H_ */
