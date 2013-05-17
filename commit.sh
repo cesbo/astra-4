6577853912b29a5841f484f4bc7dab082a7f4ff4
Andrey Dyldin <and@cesbo.com>
2013-05-17 19:15:39 +0600

    AS-88 commit.sh: fixed bugs and typos

--- commit.sh
+++ commit.sh
@@ -5,42 +5,44 @@ DEV_BRANCH="astra-4"
 usage()
 {
     cat <<EOF
-Usage: ./commit.sh options [files]
+Usage: ./commit.sh OPTIONS
+
 Options:
-    --help              This message
+    --help
+        This message
 
-    --feature MESSAGE   Commit to feature branch. Version is not changing
-    --hotfix MESSAGE    Commit to '$DEV_BRANCH'. Dev version up
+    --feature MESSAGE [FILES]
+        Commit to feature branch. Version is not changed
+    --hotfix MESSAGE [FILES]
+        Commit to '$DEV_BRANCH'. Dev version up
 
-    --major             Merge '$DEV_BRANCH' branch to 'master'
-    --minor             Merge '$DEV_BRANCH' branch to 'master'
-    --dev               Merge current branch to '$DEV_BRANCH'. Dev version up
+    --major
+        Merge '$DEV_BRANCH' branch to 'master'
+    --minor
+        Merge '$DEV_BRANCH' branch to 'master'
+    --dev
+        Merge current branch to '$DEV_BRANCH'. Dev version up
 
 Workflow:
     1. Feature
         # Start new branch
-        git checout -b AS-X
+        git checkout -b AS-X
         # Commit changes into feature branch
         ./commit.sh --feature 'AS-X changes desciption' [files]
         # Merge to dev
         ./commit.sh --dev
+        # Remove feature branch
+        git branch -d AS-X
+        # Remove remote branch if needed
+        git push origin :AS-X
     2. Hotfix
         # Commit changes into dev-branch directly
-        ./comit.sh --hotfix 'AS-X changes desciption' [files]
-    3. Releas
+        ./commit.sh --hotfix 'AS-X changes desciption' [files]
+    3. Release
         # After 'Feature' or after 'Hotfix'
-        ./commit --minor
+        ./commit.sh --minor
 EOF
     exit 1
-}
-
-check_changes()
-{
-    if ! git status | grep -q 'nothing added to commit' ; then
-        echo "Changes is not staged or not commited"
-        echo "Make commit or save it to stash"
-        exit 3
-    fi
 }
 
 [ $# -lt 1 ] && usage
@@ -52,40 +54,20 @@ if [ ! -f "commit.sh" ] ; then
     exit 2
 fi
 
-BRANCH=`git branch | grep '^\*' | cut -d ' ' -f 2`
-
-if [ "$BRANCH" != "$DEV_BRANCH" ] ; then
-    case "$1" in
-        "--dev")
-            check_changes
-            git checkout $DEV_BRANCH
-            git merge --no-ff $BRANCH --no-commit
-            ./commit.sh --hotfix "$BRANCH merge"
-            ;;
-        "--feature")
-            shift
-            MSG="$1"
-            shift
-
-            if [ $# -ne 0 ] ; then
-                git commit -m "$MSG" $*
-            else
-                git commit -am "$MSG"
-            fi
-            ;;
-        *)
-            echo "$DEV_BRANCH is required. Current branch: $BRANCH"
-            echo ""
-            usage
-            ;;
-    esac
-
-    exit
-fi
-
 ASTRA_VERSION_MAJOR=`cat version.h | grep "ASTRA_VERSION_MAJOR " | cut -d ' ' -f 3`
 ASTRA_VERSION_MINOR=`cat version.h | grep "ASTRA_VERSION_MINOR " | cut -d ' ' -f 3`
 ASTRA_VERSION_DEV=`cat version.h | grep "ASTRA_VERSION_DEV " | cut -d ' ' -f 3`
+
+BRANCH=`git branch | grep '^\*' | cut -d ' ' -f 2`
+
+check_branch()
+{
+    if [ "$BRANCH" != "$DEV_BRANCH" ] ; then
+        echo "$DEV_BRANCH is required."
+        echo "Current branch: $BRANCH"
+        exit 2
+    fi
+}
 
 version_up()
 {
@@ -102,51 +84,78 @@ EOF
     git add version.h
 }
 
+check_changes()
+{
+    if ! git status | grep -q '^nothing' ; then
+        echo "Changes is not staged or not commited"
+        echo "Make commit or save it to stash"
+        exit 3
+    fi
+}
+
 release_version()
 {
+    check_branch
     check_changes
+
+    ASTRA_VERSION_DEV=0
+    version_up
+    git commit -m "version up" version.h
 
     VERSION="v.$ASTRA_VERSION_MAJOR.$ASTRA_VERSION_MINOR"
     git checkout master
-    git merge --no-ff $DEV_BRANCH --no-commit
-
-    version_up
-
-    ./commit.sh --feature "$VERSION merge"
+    git merge --no-ff $DEV_BRANCH -m "$VERSION merge with dev"
     git tag "$VERSION"
+
     git checkout $DEV_BRANCH
 }
 
 case "$1" in
-    "--major")
-        let ASTRA_VERSION_MAJOR=ASTRA_VERSION_MAJOR+1
-        ASTRA_VERSION_MINOR=0
-        ASTRA_VERSION_DEV=0
-        release_version
-        exit
-        ;;
-    "--minor")
-        let ASTRA_VERSION_MINOR=ASTRA_VERSION_MINOR+1
-        ASTRA_VERSION_DEV=0
-        release_version
-        exit
+    "--feature")
+        shift
+        MSG="$1"
+        shift
+
+        if [ $# -ne 0 ] ; then
+            git commit -m "$MSG" $*
+        else
+            git commit -am "$MSG"
+        fi
         ;;
     "--hotfix")
         let ASTRA_VERSION_DEV=ASTRA_VERSION_DEV+1
+        version_up
+
+        shift
+        MSG="$1"
+        shift
+
+        if [ $# -ne 0 ] ; then
+            git commit -m "$MSG" $* version.h
+        else
+            git commit -am "$MSG"
+        fi
+        ;;
+    "--major")
+        let ASTRA_VERSION_MAJOR=ASTRA_VERSION_MAJOR+1
+        ASTRA_VERSION_MINOR=0
+        release_version
+        ;;
+    "--minor")
+        let ASTRA_VERSION_MINOR=ASTRA_VERSION_MINOR+1
+        release_version
+        ;;
+    "--dev")
+        check_changes
+
+        let ASTRA_VERSION_DEV=ASTRA_VERSION_DEV+1
+        version_up
+        git commit -m "version up" version.h
+
+        git checkout $DEV_BRANCH
+        git merge --no-ff $BRANCH -m "merge with $BRANCH"
         ;;
     *)
         usage
         ;;
 esac
-
-version_up
-
-shift
-MSG="$1"
-shift
-
-if [ $# -ne 0 ] ; then
-    git commit -m "$MSG" $* version.h
-else
-    git commit -am "$MSG"
-fi
