9213a8a3f990c0796b3ce11afc66f5aa40a0194c
Andrey Dyldin <and@cesbo.com>
2013-06-23 14:24:15 +0600

    AS-10 scripts/stream.lua: input/output url parser, udp_input instance list

--- scripts/stream.lua
+++ scripts/stream.lua
@@ -1,67 +1,147 @@
 
---[[
-make_channel({
-    name = "Channel 1",
-    input =
-    {
-        {
-            type = "dvb",
-            adapter = 0,
-            pnr = 100
-        }
-    },
-    output =
-    {
-        {
-            type = "udp",
-            addr = "127.0.0.1",
-            port = "10000"
-        }
-    }
-})
+function dump_table(t, p, i)
+    if not p then p = print end
+    if not i then
+        i = "    "
+        p("{")
+    end
 
-make_channel({
-    name = "Channel 2",
-    input =
-    {
-        {
-            type = "udp",
-            addr = "239.255.2.139",
-            port = 1234
-        }
-    },
-    output =
-    {
-        {
-            type = "file",
-            filename = "/dev/null"
-        }
-    }
-})
-]]--
+    for key,val in pairs(t) do
+        if type(val) == 'table' then
+            p(tostring(key) .. " = {")
+            dump_table(val, p, i .. "    ")
+        elseif type(val) == 'string' then
+            p(tostring(key) .. " = \"" .. val .. "\"")
+        else
+            p(tostring(key) .. " = " .. tostring(val))
+        end
+    end
 
-input_list = {}
-
-input_list["dvb"] = function(name, input_conf)
-    -- TODO: get dvb adapter from global lists
+    p("}")
 end
 
-input_list["udp"] = function(name, input_conf)
+-- ooooo  oooo oooooooooo  ooooo
+--  888    88   888    888  888
+--  888    88   888oooo88   888
+--  888    88   888  88o    888      o
+--   888oo88   o888o  88o8 o888ooooo88
+
+local parse_source = {}
+
+parse_source.udp = function(addr, source)
+    local x = addr:find("@")
+    if x then
+        if x > 1 then
+            source.localaddr = addr:sub(1, x - 1)
+        end
+        addr = addr:sub(x + 1)
+    end
+    local x = addr:find(":")
+    if x then
+        source.addr = addr:sub(1, x - 1)
+        source.port = tonumber(addr:sub(x + 1))
+    else
+        source.addr = addr
+        source.port = 1234
+    end
+end
+
+parse_source.file = function(addr, source)
+    source.filename = addr
+end
+
+function parse_options(options, source)
+    local x = options:find("?")
+    if x ~= 1 then
+        return
+    end
+    options = options:sub(2)
+
+    function parse_option(option)
+        local x = option:find("=")
+        if not x then return nil end
+        local key = option:sub(1, x - 1)
+        source[key] = option:sub(x + 1)
+    end
+
+    local pos = 1
+    while true do
+        local x = options:find("&", pos)
+        if x then
+            parse_option(options:sub(pos, x - 1))
+            pos = x + 1
+        else
+            parse_option(options:sub(pos))
+            return nil
+        end
+    end
+end
+
+function parse_url(url)
+    local x,_,source_type,source_addr,source_options = url:find("^(%a+)://([%a%d%.:@_-/]+)(.*)$" )
+    if not source_type then return nil end
+    if type(parse_source[source_type]) ~= 'function' then return nil end
+
+    local source = { type = source_type }
+    parse_source[source_type](source_addr, source)
+    if #source_options > 0 then parse_options(source_options, source) end
+
+    return source
+end
+
+-- ooooo oooo   oooo oooooooooo ooooo  oooo ooooooooooo
+--  888   8888o  88   888    888 888    88  88  888  88
+--  888   88 888o88   888oooo88  888    88      888
+--  888   88   8888   888        888    88      888
+-- o888o o88o    88  o888o        888oo88      o888o
+
+local input_list = {}
+
+local dvb_instance_list = {}
+
+input_list.dvb = function(name, input_conf)
+    -- TODO:
+end
+
+local udp_instance_list = {}
+
+input_list.udp = function(name, input_conf)
     if not input_conf.port then input_conf.port = 1234 end
-    return { tail = udp_input(input_conf) }
+
+    local addr = input_conf.addr .. ":" .. input_conf.port
+    if input_conf.localaddr then addr = input_conf.localaddr .. "@" .. addr end
+
+    local udp_instance
+    if udp_instance_list[addr] then
+        udp_instance = udp_instance_list[addr]
+    else
+        udp_instance = udp_input(input_conf)
+        udp_instance_list[addr] = udp_instance
+    end
+
+    return { tail = udp_instance }
 end
 
-input_list["file"] = function(name, input_conf)
+input_list.file = function(name, input_conf)
     return { tail = file_input(input_conf) }
 end
 
 function init_input(input_conf)
+    if type(input_conf) == 'string' then
+        local input_conf_t = parse_url(input_conf)
+        if not input_conf_t then
+            log.error("[stream.lua] wrong input URL format: " .. input_conf)
+            astra.abort()
+        end
+        input_conf = input_conf_t
+    end
+
     if not input_conf.type then
         log.error("[stream.lua] option 'type' is required for input")
         astra.abort()
     end
 
-    local init_input_type = input_list[input_conf.type:lower()]
+    local init_input_type = input_list[input_conf.type]
     if not init_input_type then
         log.error("[stream.lua] unknown input type")
         astra.abort()
@@ -76,9 +156,8 @@ function init_input(input_conf)
         {
             name = input_conf.name,
             upstream = input_mods.tail:stream(),
-            demux = input_mods.tail:demux(),
+            pnr = input_conf.pnr
         }
-        if input_conf.pnr then channel_conf.pnr = input_conf.pnr end
         if input_conf.caid then channel_conf.caid = input_conf.caid end
         if input_conf.sdt then channel_conf.sdt = input_conf.sdt end
         if input_conf.eit then channel_conf.eit = input_conf.eit end
@@ -88,32 +167,29 @@ function init_input(input_conf)
     end
 
     if input_conf.biss then
-        local decrypt_conf =
-        {
+        input_mods.decrypt = decrypt({
             upstream = input_mods.tail:stream(),
             biss = input_conf.biss
-        }
-
-        input_mods.decrypt = decrypt(decrypt_conf)
-        input_mods.tail = input_mods.decrypt
-    elseif input_conf.cam then
-        local  decrypt_conf =
-        {
-            upstream = input_mods.tail:stream(),
-            cam = input_conf.cam
-        }
-
-        input_mods.decrypt = decrypt(decrypt_conf)
+        })
         input_mods.tail = input_mods.decrypt
     end
 
     -- TODO: extra modules
 
-    input_mods.analyze = analyze({ upstream = input_mods.tail:stream(), name = input_conf.name })
+    input_mods.analyze = analyze({
+        upstream = input_mods.tail:stream(),
+        name = input_conf.name
+    })
     input_mods.tail = input_mods.analyze
 
     return input_mods
 end
+
+--   oooooooo8 ooooo ooooo      o      oooo   oooo oooo   oooo ooooooooooo ooooo
+-- o888     88  888   888      888      8888o  88   8888o  88   888    88   888
+-- 888          888ooo888     8  88     88 888o88   88 888o88   888ooo8     888
+-- 888o     oo  888   888    8oooo88    88   8888   88   8888   888    oo   888      o
+--  888oooo88  o888o o888o o88o  o888o o88o    88  o88o    88  o888ooo8888 o888ooooo88
 
 function make_channel(channel_conf)
     if not channel_conf.name then
