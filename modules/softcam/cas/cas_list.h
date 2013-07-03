b293d2ab3f1b818291feaff125d479374fae7396
Andrey Dyldin <and@cesbo.com>
2013-07-03 17:50:46 +0600

    AS-44 CAS-Nagra

--- modules/softcam/cas/cas_list.h
+++ modules/softcam/cas/cas_list.h
@@ -9,12 +9,13 @@ module_cas_t * irdeto_cas_init(module_decrypt_t *decrypt);
 module_cas_t * viaccess_cas_init(module_decrypt_t *decrypt);
 module_cas_t * dre_cas_init(module_decrypt_t *decrypt);
 module_cas_t * conax_cas_init(module_decrypt_t *decrypt);
+module_cas_t * nagra_cas_init(module_decrypt_t *decrypt);
 
 cas_init_t cas_init_list[] =
 {
     irdeto_cas_init,
     viaccess_cas_init,
     dre_cas_init,
-    conax_cas_init,
+    nagra_cas_init,
     NULL
 };
