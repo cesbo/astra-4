38d6643093170c60cb77bb75ca3891c2edef9c2e
Andrey Dyldin <and@cesbo.com>
2013-07-03 17:58:57 +0600

    AS-44 CAS-Videoguard

--- modules/softcam/cas/cas_list.h
+++ modules/softcam/cas/cas_list.h
@@ -10,6 +10,7 @@ module_cas_t * viaccess_cas_init(module_decrypt_t *decrypt);
 module_cas_t * dre_cas_init(module_decrypt_t *decrypt);
 module_cas_t * conax_cas_init(module_decrypt_t *decrypt);
 module_cas_t * nagra_cas_init(module_decrypt_t *decrypt);
+module_cas_t * videoguard_cas_init(module_decrypt_t *decrypt);
 
 cas_init_t cas_init_list[] =
 {
@@ -17,5 +18,6 @@ cas_init_t cas_init_list[] =
     viaccess_cas_init,
     dre_cas_init,
     nagra_cas_init,
+    videoguard_cas_init,
     NULL
 };
