b72a183829cfa4aecbb46acf3858aea54a7c3698
Andrey Dyldin <and@cesbo.com>
2013-08-05 12:41:02 +0600

    modules/softcam: FFdecsa auto-select SSE2/MMX/2INT

--- modules/softcam/module.mk
+++ modules/softcam/module.mk
@@ -5,7 +5,7 @@ SOURCES="$SOURCES_CSA $SOURCES_CAM $SOURCES_CAS decrypt.c"
 
 MODULES="decrypt newcamd"
 
-CFLAGS="-DPARALLEL_MODE=1286 -funroll-loops --param max-unrolled-insns=500"
+CFLAGS="-funroll-loops --param max-unrolled-insns=500"
 if [ "$OS" = "darwin" ] ; then
     CFLAGS="$CFLAGS -Wno-deprecated-declarations"
 fi
@@ -27,4 +27,44 @@ check_libssl()
 
 if ! check_libssl ; then
     ERROR="libssl-dev is not found"
+fi
+
+# SSE2
+
+sse2_test_c()
+{
+    cat <<EOF
+#include <stdio.h>
+#include <emmintrin.h>
+int main(void) { return 0; }
+EOF
+}
+
+check_sse2()
+{
+    sse2_test_c | $APP_C -Werror $CFLAGS $IN_CFLAGS $LDFLAGS $IN_LDFLAGS -o /dev/null -x c - >/dev/null 2>&1
+}
+
+# MMX
+
+mmx_test_c()
+{
+    cat <<EOF
+#include <stdio.h>
+#include <mmintrin.h>
+int main(void) { return 0; }
+EOF
+}
+
+check_mmx()
+{
+    mmx_test_c | $APP_C -Werror $CFLAGS $IN_CFLAGS $LDFLAGS $IN_LDFLAGS -o /dev/null -x c - >/dev/null 2>&1
+}
+
+if check_sse2 ; then
+    CFLAGS="$CFLAGS -DPARALLEL_MODE=1286"
+elif check_mmx ; then
+    CFLAGS="$CFLAGS -DPARALLEL_MODE=1284"
+else
+    CFLAGS="$CFLAGS -DPARALLEL_MODE=642"
 fi
