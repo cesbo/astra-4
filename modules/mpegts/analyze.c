a29e9d5a067827ba5c0e086ce783ab2dfea5583d
Andrey Dyldin <and@cesbo.com>
2013-08-06 11:44:37 +0600

    modules/mpegts/analyze: improve bitrate analyzer

--- modules/mpegts/analyze.c
+++ modules/mpegts/analyze.c
@@ -76,6 +76,7 @@ struct module_data_t
 
     // on_air
     uint32_t last_bitrate;
+    bool is_low_bitrate;
 };
 
 #define MSG(_msg) "[analyze %s] " _msg, mod->name
@@ -648,9 +649,17 @@ static void on_check_stat(void *arg)
     lua_setfield(lua, -2, "analyze");
 
     if(bitrate > mod->last_bitrate / 2)
+    {
         mod->last_bitrate = bitrate;
+        mod->is_low_bitrate = false;
+    }
     else if(bitrate > mod->last_bitrate / 4)
-        ;
+    {
+        if(mod->is_low_bitrate)
+            on_air = false;
+        else
+            mod->is_low_bitrate = true;
+    }
     else
         on_air = false;
 
