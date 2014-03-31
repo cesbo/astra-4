a8fb88f29040425319543926ebbf521a7041e2cb
Andrey Dyldin <and@cesbo.com>
2014-03-31 14:21:16 +0600

    version up

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define _VERSION_H_ 1
 
 #define ASTRA_VERSION_MAJOR 4
-#define ASTRA_VERSION_MINOR 0
-#define ASTRA_VERSION_DEV 349
+#define ASTRA_VERSION_MINOR 1
+#define ASTRA_VERSION_DEV 0
 
 #endif /* _VERSION_H_ */
