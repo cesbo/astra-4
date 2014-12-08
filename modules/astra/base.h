b171f5e04908b88bbb96ba9e89e75f642bd672fc
Andrey Dyldin <and@cesbo.com>
2014-12-08 21:58:00 +0500

    modules/astra/base.h: few macros

--- modules/astra/base.h
+++ modules/astra/base.h
@@ -27,6 +27,9 @@
 
 typedef struct module_data_t module_data_t;
 
+#define BUFFER_TO_U16(_b) (((_b)[0] << 8) | ((_b)[1]))
+#define BUFFER_TO_U32(_b) (((_b)[0] << 24) | ((_b)[1] << 16) | ((_b)[2] << 8) | ((_b)[3]))
+
 /* utils */
 
 char * hex_to_str(char *str, const uint8_t *hex, int len);
